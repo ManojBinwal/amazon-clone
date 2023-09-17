2d30f1d4f22714ff931788d01fcffa7a8e16fc0e
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-09-18 02:53:30 +0530

    adding header section

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB-kzPZ2_73jBxtbeXQ4YSv3B9gE0uw8U0",
+    authDomain: "challenge-6d591.firebaseapp.com",
+    projectId: "challenge-6d591",
+    storageBucket: "challenge-6d591.appspot.com",
+    messagingSenderId: "330193805704",
+    appId: "1:330193805704:web:3772df59083c98ab2b1a3d",
+    measurementId: "G-E9ECB949F3"
+  };
