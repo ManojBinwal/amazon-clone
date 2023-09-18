2bb1e901df7358fe609f569b76fc8d5fd23ba883
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-09-19 01:46:59 +0530

    completed home , product and header. Installed RRD

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import Home from './components/home/Home';
 import './App.css';
 import Header from './components/header/Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
 
   return (
     //BEM
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+
+          <Route path="/" element={[<Header />, <Home />]} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
